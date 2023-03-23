d2ebd466e978a9e4cd2dfd64cd373923f98569cf
MortalAmongGods <krmcintosh87@gmail.com>
2023-03-22 21:24:23 -0400

    added home page, my cart, and updated routing module

--- mock-store-front/src/app/app-routing.module.ts
+++ mock-store-front/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UserCartComponent } from './user-cart/user-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomePageComponent },
+  { path: 'MyCart', component: UserCartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
